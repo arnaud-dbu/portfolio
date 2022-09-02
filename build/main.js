7095f47b318b8a3fbd2511d5733ccef02fd1ecdb
ADB <arnauddbu@gmail.com>
2022-09-02 22:35:43 +0200

    mobile animations

--- build/main.js
+++ build/main.js
@@ -59,10 +59,14 @@
         },
         cacheElements() {
           this.$name = document.querySelector(".hero__name");
+          this.$primaryTitles = document.querySelectorAll(".title-primary");
+          this.$secondaryTitles = document.querySelectorAll(".title-secondary");
         },
         generateUI() {
           this.animateHero();
           this.animateTitles();
+          this.animateAbout();
+          this.animateFooter();
         },
         animateHero() {
           let mm = gsap.matchMedia();
@@ -101,19 +105,64 @@
           });
         },
         animateTitles() {
-          gsap.set(".title", {
-            x: "200vw",
-            opacity: 0
+          this.$primaryTitles.forEach((title) => {
+            gsap.set(title, {
+              x: "200vw",
+              opacity: 0
+            });
+            gsap.to(title, {
+              x: "-95vw",
+              opacity: 1,
+              scrollTrigger: {
+                trigger: title,
+                scrub: true,
+                start: "top bottom",
+                end: "bottom top"
+              }
+            });
           });
-          gsap.to(".title", {
-            x: "-95vw",
-            opacity: 1,
+          this.$secondaryTitles.forEach((title) => {
+            gsap.from(title, {
+              y: "10vw",
+              opacity: 0,
+              scrollTrigger: {
+                trigger: title,
+                start: "top bottom",
+                end: "bottom top"
+              }
+            });
+          });
+        },
+        animateAbout() {
+          gsap.from(".about__content", {
+            y: "10vh",
+            opacity: 0,
+            duration: 1,
             scrollTrigger: {
-              trigger: ".title",
-              scrub: true,
-              start: "top bottom",
-              end: "bottom top"
+              trigger: ".about__content",
+              start: "top bottom"
             }
+          });
+          gsap.from(".skill", {
+            y: "5vh",
+            opacity: 0,
+            duration: 1,
+            stagger: 0.25,
+            ease: "power4.out",
+            scrollTrigger: {
+              trigger: ".skills",
+              start: "top 65%"
+            }
+          });
+        },
+        animateFooter() {
+          gsap.set(".footer__content", {
+            x: "120vw"
+          });
+          gsap.to(".footer__content", {
+            x: "-170vw",
+            duration: 10,
+            repeat: -1
           });
         }
       };
--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -5,10 +5,15 @@ const animations = {
   },
   cacheElements() {
     this.$name = document.querySelector('.hero__name');
+    this.$primaryTitles = document.querySelectorAll('.title-primary');
+    this.$secondaryTitles = document.querySelectorAll('.title-secondary');
+
   },
   generateUI() {
     this.animateHero();
     this.animateTitles();
+    this.animateAbout();
+    this.animateFooter();
 
     // isSmall: '(min-width: 414px)',
     // isMedium: '(min-width: 810px)',
@@ -57,28 +62,82 @@ const animations = {
       opacity: 0,
       ease: "power1.out"
     })
+  },
+  animateTitles() {
+    
+    // About Title
+
+
+    this.$primaryTitles.forEach(title => {
+      
+      gsap.set(title, {
+        x: '200vw',
+        opacity: 0
+      })
+  
+      gsap.to(title, {
+        x: '-95vw',
+        opacity: 1,
+        scrollTrigger: 
+        {
+          trigger: title,
+          scrub: true,
+          start: 'top bottom',
+          end: 'bottom top'
+        }
+      }, )
+    })
+
+    // Title Secondary
+
+    this.$secondaryTitles.forEach(title => {
+      gsap.from(title, {
+        y: '10vw',
+        opacity: 0,
+  
+        scrollTrigger: {
+          trigger: title,
+          start: 'top bottom',
+          end: 'bottom top',
+        }
+      }, )
+    }) 
+  },
+  animateAbout() {
+    gsap.from('.about__content', {
+      y: "10vh",
+      opacity: 0,
+      duration: 1,
+      scrollTrigger: {
+        trigger: '.about__content',
+        start: 'top bottom',
+      }
+    })
+
+    gsap.from('.skill', {
+      y: "5vh",
+      opacity: 0,
+      duration: 1,
+      stagger: .25,
+      ease: "power4.out",
+      scrollTrigger: {
+        trigger: '.skills',
+        start: 'top 65%',
+      }
+    })
 
 
   },
-  animateTitles() {
-    gsap.set('.title',{
-      x: '200vw',
-      opacity: 0
-    }) 
+  animateFooter() {
+    gsap.set('.footer__content', {
+      x: "120vw"
+    })
 
-    gsap.to('.title', {
-      x: '-95vw',
-      opacity: 1,
-
-      scrollTrigger: {
-        trigger: '.title',
-        scrub: true,
-        start: 'top bottom',
-        end: 'bottom top'
-      }
-    },
-    )
-
+    gsap.to('.footer__content', {
+      x: "-170vw",
+      duration: 10,
+      repeat: -1
+    })
   }
 }
 
